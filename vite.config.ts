09544601bcda34828ae88c01c8ec11ee95b93752
emlznv <emlznv@gmail.com>
2026-03-05 18:46:09 +0200

    Set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
@@ -20,6 +21,11 @@ export default defineConfig({
         },
       },
     },
-    watch: {}
+    watch: {},
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+  }
 });
