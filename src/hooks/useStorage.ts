38be9e85c7b1af68b1ae629bf3cc2bffd2f3bfa7
emlznv <emlznv@gmail.com>
2026-03-15 19:04:57 +0200

    Remove unused function

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
 import * as API from '../api/api';
-import { IShow, IEpisode } from '../typescript/interfaces';
+import { IShow } from '../typescript/interfaces';
 import useSort from './useSort';
 import { Section, StorageKey } from '../typescript/enums';
 import { getFromDatabase, saveToDatabase } from '../helpers/database-helpers';
-import { isEpisodeDateToday } from '../helpers/date-helpers';
 
 const useStorage = () => {
   const [addedShows, setAddedShows] = useState<IShow[]>([]);
@@ -18,14 +17,6 @@ const useStorage = () => {
   useEffect(() => {
     getAddedShows();
   }, []);
-
-  const getEpisodeData = (previousEp?: IEpisode, nextEp?: IEpisode) => {
-    if (previousEp && !(previousEp instanceof Error) && isEpisodeDateToday(previousEp.airstamp)) {
-      return previousEp;
-    }
-
-    return nextEp instanceof Error ? undefined : nextEp;
-  };
 
   const addShow = async (show: IShow) => {
     if (!show || addedShows.some((s) => s.id === show.id)) return;
