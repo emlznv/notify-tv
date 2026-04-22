ee07818d58409ae585178ae93d3862f7e140dd49
emlznv <emlznv@gmail.com>
2026-04-22 22:57:46 +0300

    Use dev build mode to enable source maps and watch mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,33 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  build: {
-    outDir: 'build',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        background: resolve(__dirname, 'src/background.ts'),
-      },
-      output: {
-        entryFileNames: (chunkInfo) => {
-          if (chunkInfo.name === 'background') {
-            return 'background.js';
-          }
-          return '[name].js';
+export default defineConfig(({ mode }) => {
+  const isDevBuild = mode === 'development-build';
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'build',
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          background: resolve(__dirname, 'src/background.ts'),
+        },
+        output: {
+          entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name === 'background') {
+              return 'background.js';
+            }
+            return '[name].js';
+          },
         },
       },
+      sourcemap: isDevBuild,
+      minify: isDevBuild ? false : 'esbuild',
     },
-    watch: mode === 'development' ? {} : null,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts',
-  }
-}));
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/tests/setup.ts',
+    }
+  };
+});
